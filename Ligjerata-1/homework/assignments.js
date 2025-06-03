78850e049aa4d4c0261b6180c63d54f330e32a08
rbehrami <rasim.behrami@gmail.com>
2025-06-03 22:43:16 +0200

    added: in the folder Ligjerata-1 subfolder homework I have finished assignment 1

---
+++ Ligjerata-1/homework/assignments.js
@@ -0,0 +1,15 @@
+//Variables in Javascript
+//Assignments
+// 1. Create a simple profile using variables to store a person’s name, age, job, and a fun fact. Print the profile information to the console. Consider which variables will change over time and which will stay the same.
+
+//Variable to store profile information
+const fullName = "Rasim Behrami"; // it const because will not change later
+let age = 40; // The variable age is declared with let because it can be changed later.
+let job = "Front End Developer"; // The variable job is declared with let because it can be changed later.
+const funFact = "I am faster than Usain Bold."; // The variable funFact is declared with const because we are not going to change this later.
+
+console.log("My Profile");
+console.log("My name: ", fullName);
+console.log("My age: ", age);
+console.log("My job: ", job);
+console.log("My fun fact: ", funFact);
